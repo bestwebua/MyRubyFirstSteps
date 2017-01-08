bbe28e48d0c0e77e1d98405c1fad917a55c4cb60
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-01-08 16:44:13 +0200

    .

--- codewars/simple_encryption_1.rb
+++ codewars/simple_encryption_1.rb
@@ -54,3 +54,19 @@ end
 
 encrypt("This is a test!", 1)
 decrypt("bdfaceg",1)
+
+=begin
+Super refactoring code:
+
+def encrypt(text, n)
+  return text if n <= 0
+  encrypt(text.scan(/(.)(.)?/).transpose.reverse.join, n-1)
+end
+
+def decrypt(text, n)
+  return text if n <= 0
+  c, s = text.chars, text.size/2
+  decrypt(c.drop(s).zip(c.take s).join, n-1)
+end
+
+=end
