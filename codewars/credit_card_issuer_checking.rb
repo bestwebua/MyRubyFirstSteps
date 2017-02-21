aed7cd08f3e2517092db0901d487fd7d760f82d2
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-02-21 09:44:29 +0200

    .

--- codewars/credit_card_issuer_checking.rb
+++ codewars/credit_card_issuer_checking.rb
@@ -1,3 +1,36 @@
+=begin
+Credit card issuer checking by Vladislav Trotsenko.
+
+Given a credit card number we can determine who the issuer/vendor is
+with a few basic knowns.
+
++============+=============+===============+
+| Card Type  | Begins With | Number Length |
++============+=============+===============+
+| AMEX       | 34 or 37    | 15            |
++------------+-------------+---------------+
+| Discover   | 6011        | 16            |
++------------+-------------+---------------+
+| Mastercard | 51-55       | 16            |
++------------+-------------+---------------+
+| VISA       | 4           | 13 or 16      |
++------------+-------------+---------------+
+
+Write a function (getIssuer(number) (get_issuer(number) for Python))
+that will use the above known values to determine the card issuer given a card number.
+If the number cannot be matched then the function should return the string Unknown.
+
+Some sample numbers and their issuer:
+getIssuer(4111111111111111) == "VISA"
+getIssuer(4111111111111) == "VISA"
+getIssuer(4012888888881881) == "VISA"
+getIssuer(378282246310005) == "AMEX"
+getIssuer(6011111111111117) == "Discover"
+getIssuer(5105105105105100) == "Mastercard"
+getIssuer(5105105105105106) == "Mastercard"
+getIssuer(9111111111111111) == "Unknown"
+=end
+
 def getIssuer(number)
   number = number.to_s.chars.map(&:to_i)
     result = case number.size
@@ -13,7 +46,8 @@ end
 
 getIssuer(371111111111111)
 
-
+=begin
+Super refactoring code:
 def getIssuer(number)
   case number.to_s
     when /^3[47]\d{13}$/ then "AMEX"
@@ -22,3 +56,4 @@ def getIssuer(number)
     when /^4.{12,15}$/   then "VISA"
     else "Unknown"; end
 end
+=end
