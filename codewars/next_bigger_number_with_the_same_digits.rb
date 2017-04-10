9a80ba10e7127ad559cff95cb693d7d8fd140749
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-04-10 12:14:31 +0300

    .

--- codewars/next_bigger_number_with_the_same_digits.rb
+++ codewars/next_bigger_number_with_the_same_digits.rb
@@ -1,26 +1,15 @@
 
 def next_bigger(n)
   chars = n.to_s.chars
-  (0..chars.length-2).reverse_each do |i|
-    puts i
-     end
-
+    (0..chars.length-2).reverse_each do |i|
+      (i+1..chars.length-1).reverse_each do |j|
+        if chars[i] < chars[j]
+          chars[i], chars[j] = chars[j], chars[i]
+          return (chars.first(i+1)+chars.last(chars.length-1-i).sort).join.to_i
+        end
+      end
+    end
+  -1
 end
 
-p next_bigger(144)
-
-
-=begin
-
-
-
-1234567890
-1234567908
-
-def next_bigger(n)
-  size = n.to_s.size-2
-    last, new_num = n.to_s.chars.map(&:to_i).last(2).join.to_i, n.to_s[0...size]
-    last_reverse = last.to_s.reverse.to_i
-  last_reverse > last ? (new_num << last_reverse.to_s).to_i : -1
-end
-=end
+next_bigger(144)
