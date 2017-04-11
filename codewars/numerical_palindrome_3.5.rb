19453ce3d241b0dfce9e591397539373107e7af9
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-04-11 15:32:38 +0300

    .

---
+++ codewars/numerical_palindrome_3.5.rb
@@ -0,0 +1,16 @@
+=begin
+Numerical Palindrome #3.5 by Vladislav Trotsenko.
+
+=end
+
+def palindrome(num)
+  result = []
+  num = num.to_s.chars
+  num.each.with_index do |i|
+    
+  end
+end
+
+p palindrome(221122)
+
+#221122 => 11, 22, 2112, 221122
