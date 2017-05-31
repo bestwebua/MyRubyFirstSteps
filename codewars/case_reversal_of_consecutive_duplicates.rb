9d93fe3034d16d08c7068f5aa5b9a0e1ca344687
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-05-31 19:26:16 +0300

    .

--- codewars/case_reversal_of_consecutive_duplicates.rb
+++ codewars/case_reversal_of_consecutive_duplicates.rb
@@ -1,6 +1,11 @@
 =begin
 Case Reversal of Consecutive Duplicates by Vladislav Trotsenko.
 
+The aim of this Kata is to write a function which will reverse the case of all consecutive
+duplicate letters in a string. That is, any letters that occur one after the other and are identical.
+
+If the duplicate letters are lowercase then they must be set to uppercase, and if they are uppercase
+then they need to be changed to lowercase. 
 =end
 
 def reverse(str)
