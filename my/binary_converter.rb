8f22d90b7481ff120de8b1a649ea6fe0d6f8ff9b
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-09-18 15:20:48 +0300

    .

---
+++ my/binary_converter.rb
@@ -0,0 +1,16 @@
+=begin
+Binary Converter method by Vladislav Trotsenko.
+=end
+
+class Integer
+  def to_dec
+    if is_a?(Integer)
+        abort "It is not binary number!" unless !to_s.match(/[^01]/)
+      to_s.chars.map(&:to_i).inject { |memo, item| memo*2 + item }
+    else
+      "It is not number!"
+    end
+  end
+end
+
+10011011.to_dec
