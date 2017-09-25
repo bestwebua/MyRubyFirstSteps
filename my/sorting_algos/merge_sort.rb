3b7533391364145f4284c82746ec14e69ceac5df
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-09-25 11:48:48 +0300

    .

--- my/sorting_algos/merge_sort.rb
+++ my/sorting_algos/merge_sort.rb
@@ -1,6 +1,12 @@
 =begin
 Merge sort algo realization by Vladislav Trotsenko.
 
+In computer science, merge sort (also commonly spelled mergesort) is an efficient,
+general-purpose, comparison-based sorting algorithm. Most implementations produce
+a stable sort, which means that the implementation preserves the input order of
+equal elements in the sorted output. Mergesort is a divide and conquer algorithm
+that was invented by John von Neumann in 1945.
+
 The complexity of the algorithm: O(n log n).
 =end
 
