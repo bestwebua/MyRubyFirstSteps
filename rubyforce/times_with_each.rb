e8b92f72a62d6dd2cf1cc1980d096e621bf7082d
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-10-07 12:04:24 +0300

    .

--- rubyforce/times_with_each.rb
+++ rubyforce/times_with_each.rb
@@ -7,8 +7,6 @@ end
 10.times { |i| puts "I'm on iteration #{i}!" }
 
 =begin
-#Some versions
-
 class Integer
   def times(&block)
     self.positive? ? (0...self).each { |i| if block_given? ? yield(i) } : self
