757192a14592b8a287a15029a156d82ce3b92c5f
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-10-23 12:09:10 +0300

    .

--- codewars/adults-only.sql
+++ codewars/adults-only.sql
@@ -1,16 +1,17 @@
----Adults only (SQL for Beginners #1) by Vladislav Trotsenko.
+/*
+Adults only (SQL for Beginners #1) by Vladislav Trotsenko.
 
----In your application, there is a section for adults only. You need to get
----a list of names and ages of users from the users table, who are 18 years
----old or older.
+In your application, there is a section for adults only. You need to get
+a list of names and ages of users from the users table, who are 18 years
+old or older.
 
----users table schema
+users table schema
 
----name
----age
+name
+age
 
----NOTE: Your solution should use pure SQL. Ruby is used within the test cases
----to do the actual testing.
-
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases
+to do the actual testing.
+*/
 
 SELECT * FROM users WHERE age > 17
