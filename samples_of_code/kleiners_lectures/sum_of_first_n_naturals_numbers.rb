0c6ed1fb92924bd001974fd3227ea53ddb0e4e4c
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-01-27 19:52:27 +0200

    .

---
+++ samples_of_code/kleiners_lectures/sum_of_first_n_naturals_numbers.rb
@@ -0,0 +1,23 @@
+=begin
+Sum of first n naturals numbers by Vladislav Trotsenko.
+
+Динамическое программирование лекция 1 часть 5 (новый курс - простая задача)
+https://www.youtube.com/watch?v=b6_uoDsXZxs
+=end
+
+#Recursion solution
+def sum_of_first_naturals(n)
+  n == 1 ? n : sum_of_first_naturals(n-1)+n
+end
+
+#Iterative solution
+def sum_of_first_naturals(n)
+  (1..n).inject(:+)
+end
+
+#Math solution. Correct solution that works with large numeric series
+def sum_of_first_naturals(n)
+  (1+n)*n/2
+end
+
+sum_of_first_naturals(999991)
