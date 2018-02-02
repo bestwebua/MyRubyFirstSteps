9d1cb2bb8b405ca42ab3930b78ce129497afabed
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-02-02 16:23:28 +0200

    .

--- samples_of_code/algorithms_zlatopolsky_tasks/01_greatest_common_divisor.rb
+++ samples_of_code/algorithms_zlatopolsky_tasks/01_greatest_common_divisor.rb
@@ -1,5 +1,20 @@
 =begin
 Greatest common divisor, Euclidean algorithm by Vladislav Trotsenko.
+
+The algorithm for finding the GCD by dividing:
+1. The greater the number divided into smaller.
+2. If the remainder is divided, then the smaller
+number is the GCD (it is necessary to leave the loop).
+3. If there is a remainder, then a larger number is
+replaced by the remainder of the division.
+4. Proceed to step 1.
+
+Example:
+Find the GCD for 30 and 18.30/18 = 1 (residue 12)
+18/12 = 1 (balance 6)
+12/6 = 2 (remainder 0). End: 
+
+The GCD is a divisor.GCD (30, 18) = 6
 =end
 
 #My Ryby realisation of Euclidean algorithm
