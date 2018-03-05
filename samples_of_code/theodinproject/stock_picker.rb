2a904dec2f1be22f53e674637b9c40b211ce00f9
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-03-05 17:15:10 +0200

    .

--- samples_of_code/theodinproject/stock_picker.rb
+++ samples_of_code/theodinproject/stock_picker.rb
@@ -5,4 +5,14 @@ stock_picker([17,3,6,9,15,8,6,1,10])
 => [1,4]  # for a profit of $15 - $3 == $12
 =end
 
-stock_picker
+def stock_picker(stocks)
+  result = {}
+    stocks.each_with_index do |buy, buy_index|
+      stocks[buy_index+1..-1].each_with_index do |sell, sell_index|
+        result[[buy_index, sell_index+buy_index+1]] = sell-buy
+      end
+    end
+  result.max_by { |k,v| v }.first
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
