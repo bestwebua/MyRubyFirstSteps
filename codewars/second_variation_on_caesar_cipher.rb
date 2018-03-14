f1270a005b371574cccc8a9b7f698ec216acd9b6
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-03-14 18:10:13 +0200

    .

--- codewars/second_variation_on_caesar_cipher.rb
+++ codewars/second_variation_on_caesar_cipher.rb
@@ -4,15 +4,44 @@ Second Variation on Caesar Cipher by Vladislav Trotsenko.
 
 =end
 
-def encode_str(u, n)
-  dict = ('a'..'z').map { |i| i << i.upcase }
-    shift_dict = dict.rotate(n)
-  #u.tr(dict.join, shift_dict.join).split
-  p u.size/4
+def encode_str(s, shift)
+  dict = ('a'..'z').to_a
+    part_size = (s.size/5.0).ceil
+      result = s.chars.map.with_index do |char, index|
+        unless char[/[a-zA-Z]/].nil?
+          tmp_char = if index.zero?
+              dict.rotate(shift+index)[dict.index(char.downcase)]
+            else
+              dict.rotate(shift+index)[dict.rotate(index).index(char.downcase)]
+            end
+          if index.zero?
+            char.downcase + tmp_char + (char == char.upcase ? tmp_char.upcase : tmp_char)
+          else
+            char == char.upcase ? tmp_char.upcase : tmp_char
+          end 
+        else
+          char
+        end
+      end.join.scan(/.{1,#{part_size}}/)
+    result << '' if (s.size%4).zero?
+  result
 end
 
-def decode(arr)
+encode_str('I should have known that you would have a perfect answer for me!!!', 1)
 
+=begin
+def decode(arr, shift)
+  dict, s = ('a'..'z').to_a, arr.join
+    shift = s.size+shift-1
+      s.chars.reverse_each.map.with_index do |char, index|
+        unless char[/[a-zA-Z]/].nil?
+          tmp_char = dict.rotate(index-shift)[dict.index(char.downcase)]
+          char == char.upcase ? tmp_char.upcase : tmp_char
+        else
+          char
+        end
+      end.reverse.join
 end
 
 p encode_str('I should have known that you would have a perfect answer for me!!!', 1)
+=end
