8aa72726ed16fb1aacb05aa72d632bbd3f893404
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-09 14:40:55 +0300

    .

--- codewars/rock_paper_scissors.rb
+++ codewars/rock_paper_scissors.rb
@@ -3,8 +3,36 @@ Rock Paper Scissors! by Vladislav Trotsenko.
 
 =end
 
+class Player
 
+  @@total_players = 0
 
+  attr_reader :name, :beat
+  attr_accessor :winner
+
+  def initialize(beat)
+    @name = "Player #{@@total_players+=1}"
+    @beat = beat
+    @winner = nil
+  end
+
+  def lets_beat(other_player)
+    beatmap = {'paper' => 'rock', 'rock' => 'scissors', 'scissors' => 'paper'}
+    case other_player.beat
+      when beatmap[self.beat] then self.winner = true and "#{self.name} won!"
+      when beatmap.invert[self.beat] then other_player.winner = true and "#{other_player.name} won!"
+      else 'Draw!'
+    end
+  end
+
+end
+
+players = (1..6).map { |player| Player.new(['paper', 'rock', 'scissors'].sample) }
+players.each_slice(2) { |player1, player2| player1.lets_beat(player2) }
+
+p players.select(&:winner)
+
+=begin
 def rps(p1, p2)
   beatmap = {'paper' => 'rock', 'rock' => 'scissors', 'scissors' => 'paper'}
     case p2
@@ -14,10 +42,6 @@ def rps(p1, p2)
     end
 end
 
-p rps('paper', 'rock')
-
-
-=begin
 def rps(p1, p2)
   winner, players = [['paper', 'rock'], ['rock', 'scissors'], ['scissors', 'paper']], [p1, p2] 
     case
