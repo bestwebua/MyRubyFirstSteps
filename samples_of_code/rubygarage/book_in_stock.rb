e8d40c0813c535ae97b4a9aadae8d7b415dd539e
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-04-12 12:18:44 +0300

    Class instance variables fix

---
+++ samples_of_code/rubygarage/book_in_stock.rb
@@ -0,0 +1,23 @@
+class BookInStock
+  attr_reader :title, :author, :price
+
+  @count = 0
+
+  def initialize(title, author, price)
+    @title, @author, @price = title, author, Float(price)
+    self.class.add
+    # @count += 1  NoMethodError: undefined method '+' for nil:NilClass
+  end
+
+  def self.add
+    @count += 1
+  end
+
+  def self.statistics
+    'Count of add method call: ' + @count.to_s
+  end
+end
+
+5.times{ BookInStock.new('The Great Gatsby', 'F. Scott Fitzgerald', 8.99) }
+
+BookInStock.statistics # => Count of books: 5
